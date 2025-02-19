fdbfe8f802ef7a8031068215c4c6ce6fb8428cdf
Nate Marks <nmarks@imprivata.com>
2025-02-19 13:00:08 -0500

    scripts/update_cdk_libs.ah
    https://aws.amazon.com/blogs/opensource/aws-cdk-is-splitting-construct-library-and-cli/

--- scripts/update_cdk_libs.sh
+++ scripts/update_cdk_libs.sh
@@ -5,17 +5,10 @@ set -Eeuoxv pipefail
 # this is a problem on pipeline agents because the package ends up getting installed in
 # $HOME/node_modules
 
+# if the version isn't provided, install latest and check installed version
+npm install --prefix ./ aws-cdk
 
-if [[ $# -ne 1 ]]; then
-  # if the version isn't provided, install latest and check installed version
-  npm install --prefix ./ aws-cdk
-  CDK_VERSION=$(npm list --json  | jq -r '.dependencies."aws-cdk".version')
-else
-  # otherwise install provided version
-  CDK_VERSION="${1}"
-  npm install --prefix ./ "aws-cdk@${CDK_VERSION}"
-fi
-
+CDK_LIB_VERSION="$(curl -s https://pypi.org/pypi/aws-cdk-lib/json | jq -r '.info.version')"
 # remove and update aws cdk entries in requirements.txt
 sed -i '/aws-cdk-lib==/d' requirements.txt
-echo "aws-cdk-lib==${CDK_VERSION}" >> requirements.txt
+echo "aws-cdk-lib==${CDK_LIB_VERSION}" >> requirements.txt
